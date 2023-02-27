442fa6a3d2bf8134855fecaa6ae320dc937a3cba
Koen Kamman <koen@kamman.dev>
2023-02-27 22:26:15 +0100

    Configure core eslint rules

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 "use strict";
 
+const eslintJsPlugin = require("@eslint/js");
 const configPrettier = require("eslint-config-prettier");
 const pluginNoNull = require("eslint-plugin-no-null");
-
-const baseConfigs = ["eslint:recommended"];
-
-const overrideConfigs = [configPrettier];
 
 const plugins = {
   "no-null": pluginNoNull,
@@ -13,13 +10,35 @@ const plugins = {
 
 const rules = {
   "no-null/no-null": "error",
+  "no-duplicate-imports": "error",
+  "no-promise-executor-return": "error",
+  "no-self-compare": "error",
+  "no-unused-private-class-members": "error",
+  "arrow-body-style": ["error", "as-needed"],
+  camelcase: "error",
+  "default-case": "error",
+  "default-case-last": "error",
+  "default-param-last": "error",
+  "dot-notation": ["error", { allowPattern: "^[a-z]+(_[a-z]+)+$" }],
+  eqeqeq: ["error", "smart"],
+  "no-confusing-arrow": ["error", { onlyOneSimpleParam: true }],
+  "no-continue": "error",
+  "no-multi-assign": "error",
+  "no-return-assign": "error",
+  "no-unneeded-ternary": "error",
+  "no-useless-concat": "error",
+  "no-useless-return": "error",
+  "no-var": "error",
+  "prefer-const": "error",
+  "prefer-object-spread": "error",
+  "symbol-description": "error",
 };
 
-module.exports = [
-  ...baseConfigs,
-  {
-    plugins,
-    rules,
+module.exports = {
+  plugins,
+  rules: {
+    ...eslintJsPlugin.configs.recommended.rules,
+    ...rules,
+    ...configPrettier.rules,
   },
-  ...overrideConfigs,
-];
+};
